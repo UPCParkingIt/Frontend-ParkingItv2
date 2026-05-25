3508d3a1a0e44585b0ab498e5c43be88b77c9a5b
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2026-05-24 22:53:58 -0500

    fix: set baseUrl to remote DigitalOcean host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080/cloud/api/v1',
+  baseUrl: 'https://parking-cloud-i6ts5.ondigitalocean.app/cloud/api/v1',
   edgeUrl: 'http://localhost:9090/edge/api/v1',
   parkingId: '27679625-2138-438f-bbe1-4b13bb85b375',
-  yapeQrUrl: ''
+  yapeQrUrl: '',
 };
